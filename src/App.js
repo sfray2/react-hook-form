cb7e212000052b479e94fd429bcf2b077da3f227
sfray2 <shaenabfray@gmail.com>
2022-12-09 12:29:06 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import {Form} from "./components/Form";
+import {useToggle} from "./useToggle";
 
 function App() {
+
+  const [isVisible, toggle] = useToggle(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={toggle}>
+        {isVisible ? "Continue as Guest" : "Sign In"}
+      </button>
+      {isVisible && <Form />}
     </div>
   );
 }
